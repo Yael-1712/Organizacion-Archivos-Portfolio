9b1309b2efc010521e8c7055ddc9d7ea69b3c4b5
Yael-1712 <242310752@itslerdo.edu.mx>
2026-06-03 18:55:27 -0600

    Add files via upload

---
+++ Proyecto-2-NombreProyecto/Proyecto-2-NombreProyecto/src/Proyecto Corte 2/control.py
@@ -0,0 +1,4 @@
+2026-04-20 18:58:58 - ENTRADA AUTORIZADA - Ángel Adán Salas Cabello
+2026-04-20 18:59:04 - ENTRADA AUTORIZADA - Yael Alexander Calderón Reyes
+2026-04-20 18:59:11 - ENTRADA AUTORIZADA - Christian Daniel Martínez Márquez
+2026-04-20 18:59:16 - ALERTA - Intento de entrada NO AUTORIZADA
